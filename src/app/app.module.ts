70dacc89602b95e8a0d1eb9b6c0982d4eb7e0575
Vilches Gutiérrez <j.vilches.gutierrez@accenture.com>
2019-06-24 21:55:11 +0200

    LAB-03-KO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+// Modules
+import { SharedModule } from './shared/shared.module';
+import { EventsModule  } from './events/events.module';
+import { ProfileModule  } from './profile/profile.module';
+import { LoginModule  } from './login/login.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import 'hammerjs';
-import { MatCardModule } from '@angular/material/card';
-
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventDetailsComponent } from './event-details/event-details.component';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
    declarations: [
       AppComponent,
       LandingPageComponent,
       ToolbarComponent,
-      EventListComponent,
-      EventDetailsComponent
    ],
    imports: [
       BrowserModule,
-      BrowserAnimationsModule,
-      MatToolbarModule,
-      MatButtonModule,
-      MatListModule,
-      MatCardModule
+      SharedModule,
+      EventsModule,
+      LoginModule,
+      ProfileModule,
+      AppRoutingModule
    ],
    providers: [],
    bootstrap: [
--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Event } from '../models/events'
+import { Event } from '../../models/events';
 
 const EVENTS: Event[] = [
   {
